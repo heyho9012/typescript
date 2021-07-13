43381a65bf38a18da95bb98cf209603746c83f79
heyho9012 <heyho9012@gmail.com>
2021-07-13 17:45:04 +0900

    Update class

--- class-note/7_class.ts
+++ class-note/7_class.ts
@@ -1,8 +1,10 @@
 // Class
 
 class Person {
-  name: string
-  age: number
+  private name: string // priviate => class 안에서만 가능
+  public age: number
+  readonly log: string // readonly => 읽기만 가능 
+  
   constructor(name: string, age: number) {
     this.name = name
     this.age = age
